3aeeb3736e7a07bef4084f3786dd6945b88ef28a
Simon Li <spli@dundee.ac.uk>
2017-09-15 17:17:56 +0100

    Test the get_xxx token methods.

--- oauthenticator/tests/test_github.py
+++ oauthenticator/tests/test_github.py
@@ -1,8 +1,8 @@
 from pytest import fixture, mark
 
-from ..github import GitHubOAuthenticator
+from ..github import GitHubOAuthenticator, GitHubLoginHandler
 
-from .mocks import setup_oauth_mock, no_code_test
+from .mocks import setup_oauth_mock, no_code_test, mock_handler
 
 
 def user_model(username):
@@ -33,3 +33,33 @@ def test_github(github_client):
 @mark.gen_test
 def test_no_code(github_client):
     yield no_code_test(GitHubOAuthenticator())
+
+
+@mark.gen_test
+def test_github_multiple_tokens(github_client):
+    authenticator = GitHubOAuthenticator()
+    authenticator.client_id = 'id'
+    authenticator.client_id_hostmap = {
+        'a.example.org': 'ida',
+    }
+    authenticator.client_secret = 'secret'
+    authenticator.client_secret_hostmap = {
+        'a.example.org': 'secreta',
+    }
+    authenticator.oauth_callback_url = 'http://example.org/cb'
+    authenticator.oauth_callback_url_hostmap = {
+        'a.example.org': 'http://a.example.org/cba',
+    }
+
+    handler = github_client.handler_for_user(user_model('dummy'))
+    handler.request.host = 'a.example.org'
+
+    assert authenticator.get_client_id() == 'id'
+    assert authenticator.get_client_id(handler) == 'ida'
+    assert authenticator.get_client_secret() == 'secret'
+    assert authenticator.get_client_secret(handler) == 'secreta'
+    assert authenticator.get_callback_url() == 'http://example.org/cb'
+    assert authenticator.get_callback_url(handler) == \
+        'http://a.example.org/cba'
+
+    # NOTE: This doesn't test that these methods are called correctly
