660749330e63cab375eddb4c7be1e8e6b5a74ce0
Simon Li <spli@dundee.ac.uk>
2017-09-20 15:37:13 +0100

    Mark test_github_openmicroscopy xfail on travis
    This is due to travis hittin Github API rate limits

--- oauthenticator/tests/test_github_openmicroscopy.py
+++ oauthenticator/tests/test_github_openmicroscopy.py
@@ -1,4 +1,5 @@
 from pytest import mark
+from os import getenv
 
 from ..github import GitHubOrgOAuthenticator
 
@@ -6,6 +7,12 @@ from ..github import GitHubOrgOAuthenticator
 #logging.basicConfig(level=logging.DEBUG)
 
 
+# This is likely to fail on Travis due to Github rate limits:
+# https://developer.github.com/v3/#rate-limiting
+@mark.xfail(
+    getenv('TRAVIS') == 'true',
+    reason="Travis exceeds Github API rate limits"
+)
 @mark.gen_test
 @mark.parametrize("username,ismember", [
     ('not-in-org', False),
