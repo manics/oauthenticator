dec6e6efe440866b20e14c4c7dfeaeb079b7159b
Simon Li <orpheus+devel@gmail.com>
2017-10-02 16:59:34 +0100

    Elixir should now work with dev/testing site

--- oauthenticator/elixir.py
+++ oauthenticator/elixir.py
@@ -2,14 +2,15 @@
 Custom Authenticator to use Elixir AAI OAuth with JupyterHub
 
 Based on
-- oauthenticator/openshift.py
+- oauthenticator/generic.py
 - https://github.com/neicnordic/Elixir-OAuth2-python-example
 - Development Guide for ELIXIR OIDC:
   https://docs.google.com/document/d/1vOyW4dLVozy7oQvINYxHheVaLvwNsvvghbiKTLg7RbY
 """
-from base64 import urlsafe_b64decode
+import base64
 import json
 import os
+import urllib
 
 from tornado.auth import OAuth2Mixin
 from tornado import gen, web
@@ -38,13 +39,13 @@ class ElixirLoginHandler(OAuthLoginHandler, ElixirMixin):
     scope = ['profile', 'email']
 
 
-# https://github.com/jpadilla/pyjwt/blob/72bb76cb343bb6d0f40fcd0d136898b8ba08c323/jwt/utils.py#L33
-def decode_jwt_details(access_token):
-    access_token = access_token.encode('ascii').split('.')[1]
-    rem = len(access_token) % 4
+def decode_jwt_details(token):
+    # https://github.com/jpadilla/pyjwt/blob/1.5.3/jwt/utils.py#L33
+    details = token.split('.')[1]
+    rem = len(details) % 4
     if rem > 0:
-        access_token += b'=' * (4 - rem)
-    j = urlsafe_b64decode(access_token)
+        details += '=' * (4 - rem)
+    j = base64.urlsafe_b64decode(details)
     return json.loads(j)
 
 
@@ -56,22 +57,32 @@ class ElixirOAuthenticator(OAuthenticator):
 
     @gen.coroutine
     def authenticate(self, handler, data=None):
-        code = handler.get_argument("code")
+        code = handler.get_argument('code')
+        # TODO: Configure the curl_httpclient for tornado
         http_client = AsyncHTTPClient()
 
         params = dict(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            grant_type="authorization_code",
-            code=code
+            redirect_uri=self.get_callback_url(handler),
+            code=code,
+            grant_type='authorization_code',
         )
 
-        url = url_concat(ELIXIR_ACCESS_TOKEN_URL, params)
+        b64key = base64.b64encode(
+            bytes(
+                "{}:{}".format(self.client_id, self.client_secret),
+                "utf8"
+            )
+        )
 
-        req = HTTPRequest(url,
+        headers = {
+            "Accept": "application/json",
+            "User-Agent": "JupyterHub",
+            "Authorization": "Basic {}".format(b64key.decode("utf8"))
+        }
+        req = HTTPRequest(ELIXIR_ACCESS_TOKEN_URL,
                           method="POST",
-                          headers={"Accept": "application/json"},
-                          body='' # Body is required for a POST...
+                          headers=headers,
+                          body=urllib.parse.urlencode(params)
                           )
 
         resp = yield http_client.fetch(req)
@@ -79,6 +90,8 @@ class ElixirOAuthenticator(OAuthenticator):
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
 
         access_token = resp_json['access_token']
+        token_type = resp_json['token_type']
+
         details = decode_jwt_details(access_token)
         return details['sub']
 
